6c2a14a5b6ed346c1b9ac6f2eaeb4c0170718c33
vicho50 <vicente.oliva.p@ug.uchile.cl>
2026-03-19 13:14:22 -0300

    Añadida unidad pesada 'Romana'

---
+++ scripts/UnitStats.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name UnitStats
+
+@export var unit_name: String = "Unit"
+@export var max_health: int = 100
+@export var damage: int = 20
+@export var move_speed: float = 1.0
+@export var attack_range: float = 0.4
+@export var attack_cooldown: float = 1.0
+@export var team_color: Color = Color(1,1,1)
+@export var cost: int = 20
