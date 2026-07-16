11ec9c4342a232eeede05a1f9aaccde212d2a438
Tomás Lobos <tomas.lobos@ug.uchile.cl>
2026-07-16 19:51:39 -0400

    Añadidos menu y botones germanos

--- Scenes/ui.gd
+++ Scenes/ui.gd
@@ -11,12 +11,17 @@ const game_manager = preload("res://Scenes/game_manager.gd")
 @onready var Archer_Cost = $HUD/VBoxContainer/Control/HBoxContainer2/Roman_Archer_Button/HBoxContainer/Precio_Archer
 @onready var Miner_Cost = $HUD/VBoxContainer/Control/HBoxContainer2/Roman_Miner_Button/HBoxContainer/Precio_Miner
 
+
 @onready var game_over_screen: Control = $GameOverScreen
 @onready var result_label: Label = $GameOverScreen/ResultLabel
 
 @onready var Puller = $HUD/VBoxContainer/Control
 @onready var Puller_Button = $HUD/VBoxContainer/Control/TextureButton
 var is_collapsed := false
+
+@onready var Puller_2 = $HUD/VBoxContainer_2/Control_2
+@onready var Puller_Button_2 = $HUD/VBoxContainer_2/Control_2/TextureButton_2
+var is_collapsed_2 := false
 
 var _game_over := false
 
@@ -26,6 +31,7 @@ var Warrior_Mouse=load("res://Assets/UI/Mouses/Mouse_Warrior.png")
 var Archer_Mouse=load("res://Assets/UI/Mouses/Mouse_Archer.png")
 var Miner_Mouse=load("res://Assets/UI/Mouses/Mouse_Miner.png")
 var Default_Mouse=load("res://Assets/UI/Mouses/Mouse_Default.png")
+var Special_Mouse=load("res://Assets/UI/Mouses/Mouse_Special.png")
 
 var player_data
 # Tipo de unidad seleccionado para spawn al hacer click
@@ -249,6 +255,188 @@ func _on_roman_miner_button_button_up() -> void:
 	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Miner_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
 	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Miner_Button/TextureRect.scale = Vector2(1.0, 1.0)
 
+func _on_roman_special_button_pressed() -> void:
+	select_unit_to_spawn(Statics.UnitType.SPECIAL)
+	Input.set_custom_mouse_cursor(Special_Mouse)
+
+func _on_roman_special_button_mouse_entered() -> void:
+	# Set hover colors
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.scale = Vector2(1.05, 1.05)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.position -= $HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.size * 0.025
+
+func _on_roman_special_button_mouse_exited() -> void:
+	# Reset colors
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.self_modulate = Color(1.353, 1.353, 1.353)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button/TextureRect.self_modulate = Color(1.0, 1.0, 1.0)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.scale = Vector2(1.0, 1.0)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.position += $HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.size * 0.025
+
+func _on_roman_special_button_button_down() -> void:
+	# Set pressed colors
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button/TextureRect.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button/TextureRect.scale = Vector2(0.98, 0.98)
+
+func _on_roman_special_button_button_up() -> void:
+	# Revert to hover colors
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer/Control/HBoxContainer2/Roman_Special_Button/TextureRect.scale = Vector2(1.0, 1.0)
+
+
+######Germanos#######
+#Heavy
+func _on_german_heavy_button_pressed() -> void:
+	select_unit_to_spawn(Statics.UnitType.HEAVY)
+	Input.set_custom_mouse_cursor(Heavy_Mouse)
+	
+func _on_german_heavy_button_mouse_entered():
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.scale = Vector2(1.05, 1.05)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.position -= $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.size * 0.025
+	
+func _on_german_heavy_button_mouse_exited():
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.self_modulate = Color(1.353, 1.353, 1.353)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button/TextureRect.self_modulate = Color(1.0, 1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.scale = Vector2(1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.position += $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.size * 0.025
+	
+func _on_german_heavy_button_button_down() -> void:
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button/TextureRect.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button/TextureRect.scale = Vector2(0.98, 0.98)
+	
+	
+func _on_german_heavy_button_button_up() -> void:
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Heavy_Button/TextureRect.scale = Vector2(1.0, 1.0)
+	
+
+#Warrior
+# German Warrior
+func _on_german_warrior_button_pressed() -> void:
+	select_unit_to_spawn(Statics.UnitType.WARRIOR)
+	Input.set_custom_mouse_cursor(Warrior_Mouse)
+
+func _on_german_warrior_button_mouse_entered() -> void:
+	# Set hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.scale = Vector2(1.05, 1.05)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.position -= $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.size * 0.025
+func _on_german_warrior_button_mouse_exited() -> void:
+	# Reset colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.self_modulate = Color(1.353, 1.353, 1.353)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button/TextureRect.self_modulate = Color(1.0, 1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.scale = Vector2(1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.position += $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.size * 0.025
+
+func _on_german_warrior_button_button_down() -> void:
+	# Set pressed colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button/TextureRect.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button/TextureRect.scale = Vector2(0.98, 0.98)
+
+func _on_german_warrior_button_button_up() -> void:
+	# Revert to hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Warrior_Button/TextureRect.scale = Vector2(1.0, 1.0)
+# German Archer
+func _on_german_archer_button_pressed() -> void:
+	select_unit_to_spawn(Statics.UnitType.ARCHER)
+	Input.set_custom_mouse_cursor(Archer_Mouse)
+
+func _on_german_archer_button_mouse_entered() -> void:
+	# Set hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.scale = Vector2(1.05, 1.05)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.position -= $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.size * 0.025
+func _on_german_archer_button_mouse_exited() -> void:
+	# Reset colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.self_modulate = Color(1.353, 1.353, 1.353)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button/TextureRect.self_modulate = Color(1.0, 1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.scale = Vector2(1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.position += $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.size * 0.025
+
+func _on_german_archer_button_button_down() -> void:
+	# Set pressed colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button/TextureRect.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button/TextureRect.scale = Vector2(0.98, 0.98)
+
+func _on_german_archer_button_button_up() -> void:
+	# Revert to hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Archer_Button/TextureRect.scale = Vector2(1.0, 1.0)
+
+# German Miner
+func _on_german_miner_button_pressed() -> void:
+	select_unit_to_spawn(Statics.UnitType.MINER)
+	Input.set_custom_mouse_cursor(Miner_Mouse)
+
+func _on_german_miner_button_mouse_entered() -> void:
+	# Set hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.scale = Vector2(1.05, 1.05)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.position -= $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.size * 0.025
+
+func _on_german_miner_button_mouse_exited() -> void:
+	# Reset colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.self_modulate = Color(1.353, 1.353, 1.353)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button/TextureRect.self_modulate = Color(1.0, 1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.scale = Vector2(1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.position += $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.size * 0.025
+
+func _on_german_miner_button_button_down() -> void:
+	# Set pressed colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button/TextureRect.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button/TextureRect.scale = Vector2(0.98, 0.98)
+
+func _on_german_miner_button_button_up() -> void:
+	# Revert to hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Miner_Button/TextureRect.scale = Vector2(1.0, 1.0)
+
+func _on_german_special_button_pressed() -> void:
+	select_unit_to_spawn(Statics.UnitType.SPECIAL)
+	Input.set_custom_mouse_cursor(Special_Mouse)
+
+func _on_german_special_button_mouse_entered() -> void:
+	# Set hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.scale = Vector2(1.05, 1.05)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.position -= $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.size * 0.025
+
+func _on_german_special_button_mouse_exited() -> void:
+	# Reset colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.self_modulate = Color(1.353, 1.353, 1.353)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button/TextureRect.self_modulate = Color(1.0, 1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.scale = Vector2(1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.position += $HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.size * 0.025
+
+func _on_german_special_button_button_down() -> void:
+	# Set pressed colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button/TextureRect.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button/TextureRect.scale = Vector2(0.98, 0.98)
+
+func _on_german_special_button_button_up() -> void:
+	# Revert to hover colors
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button.self_modulate = Color(1.1, 1.1, 1.1)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button/TextureRect.self_modulate = Color(0.9, 0.9, 0.9)
+	$HUD/VBoxContainer_2/Control_2/HBoxContainer2/German_Special_Button/TextureRect.scale = Vector2(1.0, 1.0)
+
 
 # Recogedor
 func _on_toggle_units_button_pressed():
@@ -290,3 +478,44 @@ func _on_texture_button_button_up() -> void:
 	$HUD/VBoxContainer/Control/TextureButton.self_modulate = Color(0.8, 0.8, 0.8)
 	$HUD/VBoxContainer/Control/TextureButton.scale = Vector2(0.25, 0.25)
 	$HUD/VBoxContainer/Control/TextureButton.position -= $HUD/VBoxContainer/Control/TextureButton.size * 0.003
+	
+	# Recogedor_Germanos
+func _on_toggle_units_button_2_pressed():
+	var tween = create_tween()
+	# Get current width
+	var target_offset = -1044
+	
+	if is_collapsed:
+		# Move back to origin
+		tween.tween_property(Puller_2, "position:x", 0.0, 0.5).set_trans(Tween.TRANS_CUBIC)
+		Puller_Button_2.rotation_degrees = 0
+		
+	else:
+		# Move to edge based on width
+		tween.tween_property(Puller_2, "position:x", target_offset, 0.5).set_trans(Tween.TRANS_CUBIC)
+		Puller_Button_2.rotation_degrees = 180
+		
+	is_collapsed = !is_collapsed
+	
+func _on_texture_button_2_mouse_entered() -> void:
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.scale = Vector2(0.25, 0.25)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.position -= $HUD/VBoxContainer_2/Control_2/TextureButton_2.size * 0.002
+
+
+func _on_texture_button_2_mouse_exited() -> void:
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.self_modulate = Color(1.0, 1.0, 1.0)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.scale = Vector2(0.24, 0.24)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.position += $HUD/VBoxContainer_2/Control_2/TextureButton_2.size * 0.002
+
+func _on_texture_button_2_button_down() -> void:
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.self_modulate = Color(0.7, 0.7, 0.7)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.scale = Vector2(0.23, 0.23)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.position += $HUD/VBoxContainer_2/Control_2/TextureButton_2.size * 0.003
+
+
+func _on_texture_button_2_button_up() -> void:
+	_on_toggle_units_button_2_pressed()
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.self_modulate = Color(0.8, 0.8, 0.8)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.scale = Vector2(0.25, 0.25)
+	$HUD/VBoxContainer_2/Control_2/TextureButton_2.position -= $HUD/VBoxContainer_2/Control_2/TextureButton_2.size * 0.003
